b4b80a3922622cc2296e2582ef6caa62403ae86e
Valentin <96072753+ValenGe@users.noreply.github.com>
2022-12-14 17:46:21 +0100

    Update script_valentin.R

--- valentin/script_valentin.R
+++ valentin/script_valentin.R
@@ -22,63 +22,126 @@ left_join(
   by = "iso3c"
 ) %>%
   mutate(country = countrycode(iso3c, "iso3c", "country.name"),
-         year = ymd(year, truncated = 2L))
-
-map_plot <-
-df %>%
+         year = ymd(year, truncated = 2L),
+         point = st_point_on_surface(geometry),
+         x_coord = st_coordinates(point)[1],
+         y_coord = st_coordinates(point)[2]) %>%
   filter(continent == "Africa") %>%
   st_as_sf() %>%
+  highlight_key(~iso3c)
+  
+map_plot <-  
+  df %>%
+  filter(year == 2007) %>%
   ggplot() + 
-  geom_sf(aes(fill = gdpPercap, text = country, frame = year(year))) +
+  geom_sf(aes(fill = gdpPercap, 
+              text = paste("<b>", country, "</b>", "\n", "Year:", year(year), "\n", "GDP per capita:", round(gdpPercap, 2)))) +
   scale_fill_viridis_c(option = "viridis", 
                        trans = "log", 
-                       breaks = c(min(df$gdpPercap, na.rm = T), 
-                                median(df$gdpPercap, na.rm = T),
-                                mean(df$gdpPercap, na.rm = T),
-                                max(df$gdpPercap, na.rm = T)),
-                       labels = c(sprintf("Min: %s",min(df$gdpPercap, na.rm = T) %>% 
-                                            round(digits = 2)),
-                                sprintf("Median: %s", median(df$gdpPercap, na.rm = T) %>% 
-                                          round(digits = 2)),
-                                sprintf("Mean: %s", mean(df$gdpPercap, na.rm = T) %>% 
-                                          round(digits = 2)),
-                                sprintf("Max: %s", max(df$gdpPercap, na.rm = T) %>% 
-                                          round(digits = 2))),
                        name = "GDP per capita"
   ) +
   theme_map()
 
-ggplotly(map_plot,
-         tooltip = c("text", "fill")) %>%
-  layout(hoverlabel = list(
-    bgcolor = "white",
-    bordercolor = "transparent",
-    font = list(
-      color = "black")
-    )) %>%
-  animation_opts(transition = 0, redraw = F) %>%
+ggplotly(map_plot) %>%
+  animation_opts(transition = 0, redraw = T) %>%
   animation_slider(
     currentvalue = list(prefix = "Year: ", font = list(color = "black"))
-  )
+  ) 
 
-plot_geo(df, locationmode = "world") %>% 
-  add_trace(locations = ~iso3c, z = ~log(gdpPercap), color = ~log(gdpPercap), frame = ~year(year))
-
-plot_geo(df %>%
-           filter(year(year) == 2007)
-         ,locationmode = "world") %>% 
-  add_trace(locations = ~iso3c, z = ~log(gdpPercap), color = ~log(gdpPercap)) %>%
-  add_trace(locations = ~iso3c, z = ~log(pop), color = ~log(pop)) %>%
+plt1 <-
+plot_ly(
+  stroke = I("black"),
+  span = I(1)
+  ) %>%
+  add_sf(data = df,
+         split = ~iso3c,
+         frame = ~year(year),
+         color = ~log(gdpPercap),
+         text = ~paste("<b>", country, "</b>", "\n", "Year:", year(year), "\n", "GDP per capita:", round(gdpPercap, 2)),
+         hoveron = "fills",
+         hoverinfo = "text",
+         visible = T
+  ) %>%
+  add_sf(data = df,
+         split = ~iso3c,
+         frame = ~year(year),
+         color = ~log(pop),
+         text = ~paste("<b>", country, "</b>", "\n", "Year:", year(year), "\n", "Population:", pop),
+         hoveron = "fills",
+         hoverinfo = "text",
+         visible = F
+  ) %>%
   layout(
+    showlegend = F,
     updatemenus = list(
       list(
-        type = "list",
-        label = 'Category',
         buttons = list(
           list(method = "restyle",
-               args = list('visible', c(TRUE, FALSE)),
+               args = list('visible', c(rep(T, 48), rep(F, 49))),
                label = "gdpPercap"),
           list(method = "restyle",
-               args = list('visible', c(FALSE, TRUE)),
+               args = list('visible', c(rep(F, 48), rep(T, 49))),
                label = "pop")
-        ))))
+        )))) %>%
+  animation_opts(
+    transition = 0, redraw = T) %>%
+  animation_slider(
+    currentvalue = list(prefix = paste("<b>", "Year: "), font = list(color = "black"))
+  ) %>%
+  style(
+    hoveron = "fills",
+    hoverlabel = list(
+      bgcolor = "white",
+      bordercolor = "transparent",
+      font = list(
+        color = "black")
+    )) %>%
+  highlight(on = "plotly_hover",
+            opacityDim = .75)
+plt1
+
+plt2 <-
+  plot_ly(
+    data = df,
+    split = ~iso3c,
+    color = I("grey90"),
+    showlegend = F
+  ) %>%
+  add_markers(data = df,
+              frame = ~year(year),
+              x = ~x_coord,
+              y = ~y_coord,
+              size = ~log(pop),
+              color = ~log(pop),
+              text = ~paste("<b>", country, "</b>", "\n", "Year:", year(year), "\n", "Population:", pop),
+              hoveron = "fills",
+              hoverinfo = "text",
+              ) %>%
+  animation_opts(
+    transition = 0, redraw = T) %>%
+  animation_slider(
+    currentvalue = list(prefix = paste("<b>", "Year: "), font = list(color = "black"))
+  ) %>%
+  style(
+    hoveron = "fills",
+    hoverlabel = list(
+      bgcolor = "white",
+      bordercolor = "transparent",
+      font = list(
+        color = "black")
+    )) %>%
+  highlight(on = "plotly_hover",
+            opacityDim = .75)
+plt2
+
+plotly_json(plt1)
+
+plt <- plotly_build(plt1)
+length(plt$x$data)
+x <- invisible(lapply(1:length(plt$x$data),
+                      function(i){
+                        plt$x$data[[i]]$name
+                      })) %>% unlist()
+table(x)
+  
+
